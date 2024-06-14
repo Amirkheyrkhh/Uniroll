ebe272019545ef1212aee9649dd00eaf5b5835dd
University_Select_course <amirrezakheirkhah@yahoo.com>
2024-06-14 05:18:56 -0700

    feat: Update Course model to include terms relationship

--- model/entity/course.py
+++ model/entity/course.py
@@ -1,12 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from model.entity.base import Base
-from model.tools.validator.validator import Validator
-
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from model.entity.base import Base
-from model.tools.validator.validator import Validator
+from model.entity.term import term_course_table
 
 # Association table for the many-to-many relationship
 prerequisite_table = Table('prerequisite_tbl', Base.metadata,
@@ -24,10 +19,13 @@ class Course(Base):
     # Many-to-many self-referential relationship
     prerequisites = relationship("Course",
                                  secondary=prerequisite_table,
-                                 primaryjoin= id == prerequisite_table.c.course_id,
-                                 secondaryjoin= id == prerequisite_table.c.prerequisite_id,
+                                 primaryjoin=id == prerequisite_table.c.course_id,
+                                 secondaryjoin=id == prerequisite_table.c.prerequisite_id,
                                  backref="next_courses",
-                                 cascade="all, delete")
+                                 cascade="save-update, merge")
+    terms = relationship("Term",
+                         secondary=term_course_table,
+                         back_populates="courses")
 
     def __init__(self, name, prerequisites, professor_id, capacity, unit_count):
         super().__init__()
