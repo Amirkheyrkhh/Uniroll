d671b275a6b3acd2ec1833be16fb64b60873c5ad
University_Select_course <amirrezakheirkhah@yahoo.com>
2024-06-14 05:19:23 -0700

    refactor: Update Student model to use terms relationship instead of individual term and average_score fields

--- model/entity/student.py
+++ model/entity/student.py
@@ -1,18 +1,14 @@
-from sqlalchemy import *
+from sqlalchemy import Column, Integer
 from sqlalchemy.orm import relationship
-
-from model.entity.enrollment import enrollment_table
 from model.entity.user import User
-
+from model.entity.term import Term
 
 class Student(User):
     __mapper_args__ = {
-        'polymorphic_identity':'student',
+        'polymorphic_identity': 'student',
     }
-    term = Column(Integer, nullable=True)
-    average_score = Column(Integer, nullable=True)
+    terms = relationship("Term", back_populates="student")
 
-    def __init__(self, name, family, gender, national_code, birthday, address, phone_number, username, password, term, average_score):
-        super().__init__(name, family, gender,national_code, birthday, address, phone_number, username, password, "student")
-        self.term = term
-        self.average_score = average_score
+    def __init__(self, name, family, gender, national_code, birthday, address, phone_number, username, password, terms):
+        super().__init__(name, family, gender, national_code, birthday, address, phone_number, username, password, "student")
+        self.terms = terms
