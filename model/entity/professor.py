d0551c99f8aca03371c878233ffdc86a18bde068
University_Select_course <amirrezakheirkhah@yahoo.com>
2024-06-14 10:24:03 -0700

    refactor: Update Professor entity to use Teaching as a secondary table for courses

--- model/entity/professor.py
+++ model/entity/professor.py
@@ -1,11 +1,18 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from model.entity.teaching import Teaching
 from model.entity.user import User
 
 class Professor(User):
     __tablename__ = 'professor_tbl'
     id = Column(Integer, ForeignKey('user_tbl.id'), primary_key=True)
-    courses = relationship('Course', backref='professor')
+    courses = relationship(
+        "Course",
+        secondary=Teaching.__table__,
+        primaryjoin="Professor.id == Teaching.professor_term_id",
+        secondaryjoin="Course.id == Teaching.course_id",
+        backref="professors"
+    )
 
     def __init__(self, name, family, gender, national_code, birthday, address, phone_number, username, password):
         super().__init__(name, family, gender, national_code, birthday, address, phone_number, username, password)
